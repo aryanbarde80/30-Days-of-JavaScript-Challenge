b16a942bd5e1ae10512e2b04e5c3a02aa190b51c
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2024-09-21 09:19:26 +0530

    leetcode solutions

--- LeetCode-30DaysJS/solution22.js
+++ LeetCode-30DaysJS/solution22.js
@@ -5,14 +5,17 @@ Array.prototype.last = function() {
     // Array -> builtin object
     // prototype -> allows to apply properties to built in objects and we can use references in it like this
 
-    if(this.length == 0){
-        return -1;
-    }
-    return this[this.length-1]
+    // if(this.length == 0){
+    //     return -1;
+    // }
+    // return this[this.length-1]
 
     // this here refers to the current context that is array here
 
+    // T.C of this is O(1) bcoz we didn't made any new data structure here
 
+    // solution using ternary operator
+    return this.length ? this[this.length-1] : -1
 
 };
 

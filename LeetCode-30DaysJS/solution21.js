39c8217b73fbadc082429c4ea8e21b817fc9194c
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2024-09-21 09:15:31 +0530

    leetcode solutions

--- LeetCode-30DaysJS/solution21.js
+++ LeetCode-30DaysJS/solution21.js
@@ -3,12 +3,21 @@
  * @param {number} size
  * @return {Array}
  */
-var chunk = function(arr, size) {
-    let result = [] // blank array declaration
+// var chunk = function(arr, size) {
+//     let result = [] // blank array declaration
 
-    for(let i = 0; i < arr.length ; i+= size){
-        result.push(arr.slice(i,i + size))
+//     for(let i = 0; i < arr.length ; i+= size){
+//         result.push(arr.slice(i,i + size))
+//     }
+
+//     return result
+// };
+
+var chunk = function(arr,size){
+    const result = []
+    
+    while(arr.length>0){
+        result.push(arr.splice(0,size));
     }
-
     return result
-};
+}
